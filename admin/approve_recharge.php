ea212aecfccb8de778fcea8ec100d9d873f7fd42
Aasif Ahmed <asifarslaan@gmail.com>
2023-03-16 16:02:29 +0530

    recharge section done

--- admin/approve_recharge.php
+++ admin/approve_recharge.php
@@ -1,4 +1,9 @@
-<?php include('header.php'); ?>
+<?php 
+session_start();
+error_reporting(E_ALL & ~E_NOTICE);
+
+if ($_SESSION['ID'] > 0) {
+include('header.php'); ?>
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
@@ -121,4 +126,8 @@
 <div class="clearfix"></div>
 
 
-<?php include('footer.php'); ?>
+<?php include('footer.php');
+} else {
+  echo "<script>window.location='adminLogin.php' </script>";
+}
+?>
--- admin/manage_user.php
+++ admin/manage_user.php
@@ -1,4 +1,9 @@
-<?php include('header.php');?>
+<?php 
+session_start();
+error_reporting(E_ALL & ~E_NOTICE);
+
+if ($_SESSION['ID'] > 0) {
+include('header.php');?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -139,4 +144,8 @@
 	</script>	<script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <div class="clearfix"></div>
 
-<?php include('footer.php');?>
+<?php include('footer.php');
+} else {
+  echo "<script>window.location='adminLogin.php' </script>";
+}
+?>
