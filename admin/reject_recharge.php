9759c137807728e43bc12b5e4bb41cb2b8e36e59
Aasif Ahmed <asifarslaan@gmail.com>
2023-03-16 15:36:29 +0530

    recharge section done

--- admin/reject_recharge.php
+++ admin/reject_recharge.php
@@ -1,95 +1,117 @@
-<?php include('header.php');?>
+<?php include('header.php'); ?>
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-     <br></br>
-      <ol class="breadcrumb">
-        <li><a href="desktop.php"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Manage Recharge Rejected</li>
-      </ol>
-    </section>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <br></br>
+    <ol class="breadcrumb">
+      <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active"> Rejected Recharge Records </li>
+    </ol>
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          
+  <!-- Main content -->
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
 
-          <div class="box">
-            <div class="box-header box-header2">
+
+        <div class="box">
+          <div class="box-header box-header2">
             <div class="col-xs-6 text-right">
-         <h3 class="box-title"></h3>
-         </div>
-              <div class="col-sm-6">
-              <div class="pull-right">&nbsp;</div>
-       </div>
-      
+              <h3 class="box-title"></h3>
             </div>
-            <!-- /.box-header -->
-            <div class="box-body" style="overflow-x:auto;">
-  <form id="formID" name="formID" method="post" action="#" enctype="multipart/form-data">
-          <!--<div class="table-responsive"> -->
+            <div class="col-sm-6">
+              <div class="pull-right">&nbsp;</div>
+            </div>
+
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body" style="overflow-x:auto;">
+            <form id="formID" name="formID" method="post" action="#" enctype="multipart/form-data">
+              <!--<div class="table-responsive"> -->
               <table id="example1" class="table table-striped">
-                  <h4 class="table_bg ">Manage Recharge Rejected</h4>
-              	 
-              	 </br>
-              	 
+                <h4 class="table_bg ">Rejected Recharge Records</h4>
+
+                </br>
+
                 <thead>
-                <tr>
-                <th>Sr.No</th>
-                  <th>User Name</th>
-                <th>User Mobile</th>
-                <th>Amount</th>
-                 <!--   <th>Payment Type</th> -->
-                <th>TXN No.</th>
-                  <th>Image</th>
-                <th>Date</th>
-                 
-                 <th>Status</th>
-                </tr>
+                  <tr>
+                    <th>#</th>
+                    <th>User Name</th>
+                    <th>User Mobile</th>
+                    <th>Amount</th>
+                    <!--   <th>Payment Type</th> -->
+                    <th>TXN No.</th>
+                    <!-- <th>Image</th> -->
+                    <th>Date</th>
+
+                    <th>Status</th>
+                    
+                  </tr>
                 </thead>
                 <tbody>
-       
-                  <tr>
-                  <td>2940</td>
-                     <td></td>
-              <td>8881145597</td>
-              <td>500</td>
-                <!--  <td>EKPAY</td> -->
-               <td>127929929254</td>
-                       <td><a href="https://glamourclub.in/alex/uploads/" target="_blank"><img src="uploads/" height="50px" alt=""></a></td>
-              <td>07:43 AM 25-02-2023</td>
-			 
-              <td>Rejected</td>
-                </tr>
-          
-       
+                  <?php
+                  $count = 1;
+                  include('connection.php');
+                  $sql = "select * from recharge_details where status=-2 ";
+                  $result = mysqli_query($connection, $sql);
+                  if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                      // print_r($row);
+                      ?>
+
+
+                      <tr>
+                        <td>
+                          <?php echo $count++; ?>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                          <?php echo $row['recharge_amount']; ?>
+                        </td>
+                        <td>
+                          <?php echo $row['transaction_number'] ?>
+                        </td>
+                        <td>
+                          <?php echo $row['updated_at']; ?>
+                        </td>
+                        <td>
+                          <?php
+                          if ($row['status'] ==-2 ) {
+                            echo "Rejected";
+                          } 
+                          ?>
+
+                      </tr>
+                    <?php }
+                  } ?>
+
                 </tbody>
               </table>
               <!--</div>-->
-   <div class="box-header box-header2" style="margin-bottom: 10px;">&nbsp; </div>
-<div class="row">              
-<div class="col-sm-6"></div>
-              <div class="col-sm-6 text-right">
-<!-- <strong>Total Request Amount: <i class="red_txt">572,637.00</i></strong> -->
-            </div>
+              <div class="box-header box-header2" style="margin-bottom: 10px;">&nbsp; </div>
+              <div class="row">
+                <div class="col-sm-6"></div>
+                <div class="col-sm-6 text-right">
+                  <!-- <strong>Total Request Amount: <i class="red_txt">1,028,120.00</i></strong> -->
+                </div>
               </div>
-              </form>
-            </div>
-            <!-- /.box-body -->
+            </form>
           </div>
-          <!-- /.box -->
+          <!-- /.box-body -->
         </div>
-        <!-- /.col -->
+        <!-- /.box -->
       </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
 <div class="clearfix"></div>
 
-<?php include('footer.php');?>
 
-
+<?php include('footer.php'); ?>
